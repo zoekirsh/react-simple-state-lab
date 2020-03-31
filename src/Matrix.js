6a090c26b70a26a590b6678ea32a70a596e494c9
github username <github email address>
2020-03-31 10:26:34 -0400

    fixed issue where genMatrix and genRow where not returning anything.

--- src/Matrix.js
+++ src/Matrix.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
   }
   
   genMatrix = () => {
-    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+    return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
   render() {
